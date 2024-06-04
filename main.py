2edde7ef6f2eb4229ef13b6888b1727759f9f43b
unbroken2650 <unbroken2650@gmail.com>
2024-06-04 18:28:27 +0900

    undergrad grad 분리

--- main.py
+++ main.py
@@ -10,30 +10,46 @@ from dotenv import load_dotenv
 load_dotenv()
 SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL")
 CSV_FILE_INFOCOM = 'database/notified_posts_infocom.csv'
+CSV_FILE_INFOCOM_GRAD = 'database/notified_posts_infocom_grad.csv'
 CSV_FILE_SCATCH = 'database/notified_posts_scatch.csv'
 CSV_FILE_DISU = 'database/notified_posts_disu.csv'
-TIME_INTERVAL = 5
 
 
 def check_new_posts_infocom():
-    base_urls = [
-        {'name': '학사', 'link': 'http://infocom.ssu.ac.kr/kor/notice/undergraduate.php'},
-        {'name': '대학원', 'link': 'http://infocom.ssu.ac.kr/kor/notice/graduateSchool.php'}
-    ]
-    for url in base_urls:
-        response = requests.get(url['link'])
-        soup = BeautifulSoup(response.text, 'html.parser')
+    url_origin = 'http://infocom.ssu.ac.kr/kor/notice/undergraduate.php'
 
-        posts = soup.find_all('div', class_='subject on')
+    response = requests.get(url_origin)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-        new_posts = []
-        for post in posts:
-            title_span = post.find('span')
-            title = f"[{url['name']}] {title_span.text.strip('[대학원] ')}" if title_span else None
-            link = post.find_parent('a')['href'] if post.find_parent('a') else ""
-            full_link = f'http://infocom.ssu.ac.kr{link}' if link.startswith(
-                '/') else link if link != "" else url['link']
-            new_posts.append({'title': title, 'link': full_link})
+    posts = soup.find_all('div', class_='subject on')
+
+    new_posts = []
+    for post in posts:
+        title_span = post.find('span')
+        title = title_span.text if title_span else None
+        link = post.find_parent('a')['href'] if post.find_parent('a') else ""
+        full_link = f'http://infocom.ssu.ac.kr{link}' if link.startswith(
+            '/') else link if link != "" else url_origin
+        new_posts.append({'title': title, 'link': full_link})
+
+    return new_posts
+
+
+def check_new_posts_infocom_grad():
+    url_origin = 'http://infocom.ssu.ac.kr/kor/notice/graduateSchool.php'
+    response = requests.get(url_origin)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    posts = soup.find_all('div', class_='subject on')
+
+    new_posts = []
+    for post in posts:
+        title_span = post.find('span')
+        title = title_span.text if title_span else None
+        link = post.find_parent('a')['href'] if post.find_parent('a') else ""
+        full_link = f'http://infocom.ssu.ac.kr{link}' if link.startswith(
+            '/') else link if link != "" else url_origin
+        new_posts.append({'title': title, 'link': full_link})
 
     return new_posts
 
@@ -100,13 +116,14 @@ def send_slack_message(title, link, source, color):
 
 def load_notified_posts(csv_file):
     notified_posts = set()
-    try:
+    if os.path.exists(csv_file):
         with open(csv_file, mode='r', newline='', encoding='utf-8') as file:
             reader = csv.reader(file)
-            for row in reader:
-                notified_posts.add(row[0])
-    except FileNotFoundError:
-        pass
+            notified_posts = set(row[0] for row in reader)
+
+        sorted_posts = sorted(notified_posts)
+        save_notified_posts(sorted_posts, csv_file)
+
     return notified_posts
 
 
@@ -119,16 +136,16 @@ def save_notified_posts(notified_posts, csv_file):
 
 def notify_new_posts(new_posts, source, csv_file, color):
     notified_posts = load_notified_posts(csv_file)
-    new_notifications = 0
-
+    notified = False
     for post in new_posts:
-        if post['link'] not in notified_posts:
+        if post['link'] and post['link'] not in notified_posts:
+            notified = True
             send_slack_message(post['title'], post['link'], source, color)
             notified_posts.add(post['link'])
-            new_notifications += 1
-            time.sleep(TIME_INTERVAL)
+            time.sleep(5)
+            break
 
-    if new_notifications == 0:
+    if not notified and not any(post['link'] and post['link'] not in notified_posts for post in new_posts):
         send_slack_message("새로운 공지사항이 없습니다.", None, source, '#aaaaaa')
 
     save_notified_posts(notified_posts, csv_file)
@@ -136,11 +153,14 @@ def notify_new_posts(new_posts, source, csv_file, color):
 
 if __name__ == "__main__":
     new_posts_infocom = check_new_posts_infocom()
+    new_posts_infocom_grad = check_new_posts_infocom_grad()
     new_posts_scatch = check_new_posts_scatch()
     new_posts_disu = check_new_posts_disu()
-    notify_new_posts(new_posts_infocom, "전자정보공학부", CSV_FILE_INFOCOM, '#941b22')
-    time.sleep(TIME_INTERVAL)
+    notify_new_posts(new_posts_infocom, "전자정보공학부 학사", CSV_FILE_INFOCOM, '#941b22')
+    time.sleep(5)
+    notify_new_posts(new_posts_infocom_grad, "전자정보공학부 대학원", CSV_FILE_INFOCOM_GRAD, '#941b22')
+    time.sleep(5)
     notify_new_posts(new_posts_scatch, "SSU:catch", CSV_FILE_SCATCH, '#016694')
-    time.sleep(TIME_INTERVAL)
+    time.sleep(5)
     notify_new_posts(new_posts_disu, "차세대반도체학과", CSV_FILE_DISU, '#2596be')
     print(f"Checked for new posts at {datetime.now()}")
